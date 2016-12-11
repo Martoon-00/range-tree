61746aa7f9ca0d03505ddd177a57db4f33135488
Martoon <Martoon_00@mail.ru>
2016-12-11 19:12:46 +0300

    Fix datatypes

--- src/Data/Range/Tree.hs
+++ src/Data/Range/Tree.hs
@@ -6,7 +6,6 @@ module Data.Range.Tree
 
 import           Control.Lens  (Index, IxValue, Ixed (..), iso)
 import qualified Data.Foldable as F
-import           Data.Monoid   ((<>))
 import           Data.Vector   (Vector)
 import           GHC.Exts      (IsList (..))
 
@@ -34,16 +33,18 @@ type Splitter c = Point c -> PathChoice
 
 -- * Tree
 
-data Tree p = Leaf p
-            | Node (Splitter (IxValue p)) (Tree p) (Tree p)
-            | Sublayer (Tree p)
+data Tree p
+    = Nil
+    | Node
+    { splitter :: (Splitter (IxValue p))
+    , content  :: (Vector p)
+    , left     :: (Tree p)
+    , right    :: (Tree p)
+    , subtree  :: Maybe (Tree p)
+    }
 
 instance F.Foldable Tree where
-    foldMap f t = doFold t
-      where
-        doFold (Leaf p)      = f p
-        doFold (Node _ l r)  = doFold l <> doFold r
-        doFold (Sublayer t') = doFold t'
+    foldMap f = foldMap f . content
 
 instance (p ~ Point c) => IsList (Tree p) where
     type Item (Tree p) = p

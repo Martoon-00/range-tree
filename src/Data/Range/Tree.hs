abc2bc0d554620cb467934f6422ad24f671378e3
Martoon <Martoon_00@mail.ru>
2016-12-11 18:58:59 +0300

    Add relared datatypes

--- src/Data/Range/Tree.hs
+++ src/Data/Range/Tree.hs
@@ -1,3 +1,54 @@
+{-# LANGUAGE TypeFamilies #-}
+
 module Data.Range.Tree
     (
     ) where
+
+import           Control.Lens  (Index, IxValue, Ixed (..), iso)
+import qualified Data.Foldable as F
+import           Data.Monoid   ((<>))
+import           Data.Vector   (Vector)
+import           GHC.Exts      (IsList (..))
+
+-- * Datatypes
+
+-- ** Point
+
+newtype Point c = Point
+    { coords :: Vector c
+    } deriving (Show)
+
+instance Ixed (Point c) where
+    ix i = iso coords Point . ix i
+
+type instance IxValue (Point c) = c
+type instance Index (Point c) = Int
+
+
+-- * Splitter
+
+data PathChoice = Left | Right
+
+type Splitter c = Point c -> PathChoice
+
+
+-- * Tree
+
+data Tree p = Leaf p
+            | Node (Splitter (IxValue p)) (Tree p) (Tree p)
+            | Sublayer (Tree p)
+
+instance F.Foldable Tree where
+    foldMap f t = doFold t
+      where
+        doFold (Leaf p)      = f p
+        doFold (Node _ l r)  = doFold l <> doFold r
+        doFold (Sublayer t') = doFold t'
+
+instance (p ~ Point c) => IsList (Tree p) where
+    type Item (Tree p) = p
+    fromList = buildTree
+    toList = F.toList
+
+buildTree :: [Point c] -> Tree (Point c)
+buildTree = undefined
